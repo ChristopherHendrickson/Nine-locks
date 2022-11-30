41906aa8e2df48af970e58ea36d84705b0cfd993
christopherhendricksonga <christopher.hendricksonacc@gmail.com>
2022-11-30 23:27:05 +1300

    deploy fix

--- auth.py
+++ auth.py
@@ -14,20 +14,9 @@ def register():
             "username":username,
             "id":id
         }
-    session['current_user'] = user_obj
     return jsonify({
         "success":"success",
         "message":"Successfully registered",
         "user":user_obj
     })
 
-@auth_router.route('/api/verify/', methods=['GET'])
-def verify():
-    current_user = session.get('current_user', None)
-    if not current_user:
-        abort(404, 'User not logged in or not found')
-    return jsonify({
-        'status': 'success',
-        'message': 'User verified',
-        'user': current_user
-    })
